3e6b6be4ed385667b6fd50c077cf8c7c44eb70a1
Nguyen Truong <ntruong9@csc.com>
2017-12-21 09:41:35 +0700

    update css

--- src/app/shared/right-menu/right-menu.component.ts
+++ src/app/shared/right-menu/right-menu.component.ts
@@ -8,66 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class RightMenuComponent implements OnInit {
 
   mainMenuItems = [{
-    title: 'menuCustomerList',
-    icon: 'sync',
-    state: 'app.customerList',
-    class: '',
-    protect: false // true / false
+    title: 'Home'
   },
   {
-    title: 'advisorProfile',
-    icon: 'face',
-    state: 'app.advisorProfile',
-    class: '',
-    protect: false // true / false
+    title: 'About'
   },
   {
-    title: 'productList',
-    icon: 'security',
-    state: 'app.productList',
-    class: '',
-    protect: false // true / false
-  },
-  {
-    title: 'proposalList',
-    icon: 'security',
-    state: 'app.proposalList',
-    class: 'disable-click',
-    protect: false
-  },
-  {
-    title: 'resourceLibrary',
-    icon: 'info',
-    state: 'app.resourceLibrary',
-    class: '',
-    protect: false // true / false
-  }, {
-    title: 'referralList',
-    icon: 'info',
-    state: 'app.referralList',
-    class: '',
-    protect: false // true / false
-  },
-  {
-    title: 'applicationList',
-    icon: 'info',
-    state: 'app.myapplicationList',
-    class: 'disable-click',
-    protect: false
-  },
-  {
-    title: 'settings',
-    icon: 'android',
-    state: 'app.settings',
-    class: '',
-    protect: false // true / false
-  },
-  {
-    title: 'support',
-    icon: 'info',
-    state: 'app.support',
-    class: 'disable-click',
-    protect: false
+    title: 'Contact'
   }
   ];
   constructor() { }
